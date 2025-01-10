c22d7d23745be0a57e8375034908d5b97be5e6a8
Pranav Bheemsetty <pranavsetty@gmail.com>
2025-01-10 19:08:38 +0000

    Externalize react-router-dom

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Add this line to specify the output directory
+    outDir: "build",
+    rollupOptions: {
+      external: ["react-router-dom"],
+    },
   },
 });
